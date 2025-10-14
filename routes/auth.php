49fd0fa1e7a915d0d03b72e9666ec80e2ee54016
eduMagro <edumagrolemus@hotmail.com>
2025-10-14 17:37:50 +0200

    corrijo rutas auth

--- routes/auth.php
+++ routes/auth.php
@@ -11,13 +11,12 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Solo invitados (no autenticados)
-|--------------------------------------------------------------------------
-*/
-
 Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
@@ -36,18 +35,7 @@ Route::middleware('guest')->group(function () {
         ->name('password.store');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Solo usuarios autenticados
-|--------------------------------------------------------------------------
-*/
 Route::middleware('auth')->group(function () {
-    // ✅ Ahora el registro solo lo puede hacer alguien logueado
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
     Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
 
@@ -64,8 +52,7 @@ Route::middleware('auth')->group(function () {
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-    Route::put('password', [PasswordController::class, 'update'])
-        ->name('password.update');
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
