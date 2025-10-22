6bb5d0bc5b812b9b72651b96ab8c084c795b8335
eduMagro <edumagrolemus@hotmail.com>
2025-10-22 15:18:06 +0200

    arreglo logotipo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,7 @@
-<h1 class="text-xl md:text-2xl font-light tracking-wide text-black text-center">
-    Casa Cortijo <span class="font-semibold">Olivar</span>
-</h1>
+<div class="flex items-center justify-center gap-3">
+    <img src="{{ asset('imagenesWeb/ico/favicon-96x96.png') }}" alt="Logotipo Casa Cortijo Olivar"
+        class="w-10 h-10 md:w-12 md:h-12">
+    <h1 class="text-xl md:text-2xl font-light tracking-wide text-black">
+        Casa Cortijo <span class="font-semibold">Olivar</span>
+    </h1>
+</div>
